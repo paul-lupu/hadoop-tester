600abd5ed9742c539c68772e9e8422ceb1c94b91
paul-lupu <lupugeorgepaul@gmail.com>
2015-09-10 12:26:42 +0300

    patch

--- tests_ht/manifests/api.pp
+++ tests_ht/manifests/api.pp
@@ -2,8 +2,13 @@ class tests_ht::api(
 $catalog,
 $apis,
 ){
-	file {'/root/api_tests':
-		content	=> template('tests_ht/api_tests.erb'),
+	file {'/opt/tests/api_spec.js':
+		content	=> template('tests_ht/api_test.erb'),
 		ensure	=> present, 
+	}
+	exec {'node-jasmine':
+		provider		=> 'shell', 
+		command			=> 'jasmine-node --forceexit --verbose /opt/tests/api_spec.js',
+		require			=> File['/opt/tests/api_spec.js'],
 	}
 }
--- tests_ht/manifests/package.pp
+++ tests_ht/manifests/package.pp
@@ -2,9 +2,24 @@ class tests_ht::package{
 	package{'npm':
 		ensure	=> present
 	}
+  file {'test_dir':
+    path      => "/opt/tests",
+    ensure    => directory
+  }
+	exec{'install jasmine':
+		provider		=> 'shell', 
+		command			=> 'cd /opt/tests && npm install jasmine-node',
+		require			=> [File['test_dir'],Package['npm']]
+	}
 	exec{'install frisby':
 		provider		=> 'shell', 
-		command			=> 'npm install -g jasmine-node',
-		require			=> Package['npm']
+		command			=> 'cd /opt/tests && sudo npm install --save-dev frisby',
+		require			=> [File['test_dir'],Package['npm']]
 	}
+  exec{'form-data':
+    provider    => 'shell',
+    command     => 'cd /opt/tests && npm install form-data',
+    require     => [File['test_dir'],Package['npm']]
+  }
+
 }
