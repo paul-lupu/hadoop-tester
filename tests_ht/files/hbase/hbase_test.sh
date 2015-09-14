4959f17f361659d27f31b8a4f75a5c6b971368d8
paul-lupu <lupugeorgepaul@gmail.com>
2015-09-14 16:50:54 +0300

    added exit statuses to scripts

--- tests_ht/files/hbase/hbase_test.sh
+++ tests_ht/files/hbase/hbase_test.sh
@@ -4,7 +4,9 @@ echo " ##### RUNNING HBASE TABLE LIST TEST ##### "
 hbase shell hbase/hbase_commands
 if [ $? -eq 0 ]; then
 	echo "Test completed successfully."
+	exit 0
 else
 	echo "Test failed."
+	exit 1
 fi
 
--- tests_ht/files/hive/hive_test.sh
+++ tests_ht/files/hive/hive_test.sh
@@ -4,7 +4,9 @@ echo " ##### RUNNING HIVE TABLE LIST TEST ##### "
 su - hive -c "hive -e 'show tables;'"
 if [ $? -eq 0 ]; then
         echo "Test completed successfully."
+				exit 0
 else
         echo "Test failed."
+				exit 1
 fi
 
