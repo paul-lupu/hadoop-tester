033357e7cb58f77abb7073e8dc55bce454ef4f64
paul-lupu <lupugeorgepaul@gmail.com>
2015-09-15 18:22:40 +0300

    added test user for testing sandbox

--- tests_ht/manifests/api.pp
+++ tests_ht/manifests/api.pp
@@ -6,4 +6,16 @@ $apis,
 		content	=> template('tests_ht/api_test.erb'),
 		ensure	=> present, 
 	}
+	user{'unit':
+		name		=> 'unit',
+		ensure	=> 'present', 
+		shell 	=> '/bin/bash',
+		password	=> '$6$Qj/Fx6lA$S0N.PnGLBQqAQnkSN8elDFOt0Ncqh4gcFHUgNNEmOiviN8dbWBQRVDVJWHwvLvBqZXaOm5.dmzhIB9frKZ5Up1',
+		comment		=> 'Unit user for doing unit tests',
+	}
+	exec{'expire_unit':
+		provider		=> 'shell',
+		command			=> ' chage -E `date -d "1 day" +"%Y-%m-%d"` unit',
+		require			=> User['unit'],
+	}
 }
