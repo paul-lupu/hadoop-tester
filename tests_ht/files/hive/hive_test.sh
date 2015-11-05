840e302705482805494964318e99bcedb3124047
paul-lupu <lupugeorgepaul@gmail.com>
2015-11-05 14:35:16 +0200

    fixed hive test script

--- tests_ht/files/hive/hive_test.sh
+++ tests_ht/files/hive/hive_test.sh
@@ -4,19 +4,17 @@ echo " ##### RUNNING HIVE TABLE LIST TEST ##### "
 /usr/bin/timeout 400s /usr/bin/hive -e 'show tables;'
 if [ $? -eq 0 ]; then
         echo "Test completed successfully."
-				exit 0
 else
         echo "Test failed."
 				exit 1
 fi
 echo ""
-/usr/bin/timeout 400s su - hive -c "/usr/bin/hive -e 'select count(*) from sample_07;'"
+/usr/bin/timeout 400s /usr/bin/hive -e 'select count(*) from sample_07;'
 if [ $? -eq 0 ]; then
         echo "Test completed successfully."
-        exit 0
 else
         echo "Test failed."
         exit 1
 fi
-
+exit 0 
 
