17961af01a01c8be8a8cf321e22c72ba3e6468ea
paul-lupu <lupugeorgepaul@gmail.com>
2015-11-19 10:12:00 +0200

    removed su - hdfs from sqoop script

--- tests_ht/files/sqoop/sqoop_test.sh
+++ tests_ht/files/sqoop/sqoop_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "##### TESTING SQOOP EXPORT #####"
 mysql -u root -e 'use test; create table sample_07 (code text, description text, total_emp int, salary int);'
-su - hdfs -c 'sqoop export --connect jdbc:mysql://127.0.0.1/test --username hive --password hive --table sample_07 --export-dir /apps/hive/warehouse/sa' 
+sqoop export --connect jdbc:mysql://127.0.0.1/test --username hive --password hive --table sample_07 --export-dir /apps/hive/warehouse/sample_07
 mysql -u root -e 'use test; drop table sample_07;'
 if [ $? -ne 0 ]; then
 	exit 1
