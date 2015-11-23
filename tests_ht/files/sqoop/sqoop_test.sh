17a5441074ad94bcb979499fca5bc2d537363fe6
paul-lupu <lupugeorgepaul@gmail.com>
2015-11-23 14:53:27 +0200

    fixed sqoop test

--- tests_ht/files/sqoop/sqoop_test.sh
+++ tests_ht/files/sqoop/sqoop_test.sh
@@ -2,7 +2,6 @@
 echo "##### TESTING SQOOP EXPORT #####"
 mysql -u root -e 'use test; create table sample_07 (code text, description text, total_emp int, salary int);'
 sqoop export --connect jdbc:mysql://127.0.0.1/test --username hive --password hive --table sample_07 --export-dir /apps/hive/warehouse/sample_07
-mysql -u root -e 'use test; drop table sample_07;'
 if [ $? -ne 0 ]; then
 	exit 1
 fi
