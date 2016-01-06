026fe25158c28fca386324ce9d8092f19df76dd6
paul-lupu <lupugeorgepaul@gmail.com>
2016-01-06 17:29:47 +0200

    updated hbase tester

--- tests_ht/files/hbase/hbase_test.sh
+++ tests_ht/files/hbase/hbase_test.sh
@@ -4,7 +4,7 @@ echo  " ##### SENDING HBASE START COMMAND AND WAITING FOR SERVICE TO START #####
 
 curl  -s -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo":{"context":"Turn Off Maintenance Mode"},"Body":{"ServiceInfo":{"maintenance_state":"OFF"}}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE
 sleep 5
-until curl  -s -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"ServiceInfo": {"state" : "STARTED"}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE | grep Accepted; do sleep 2; done;
+curl  -s -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"ServiceInfo": {"state" : "STARTED"}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE
 until /usr/bin/curl -s --user admin:admin -H "X-Requested-By: ambari" http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE | grep -P '"CRITICAL" : 0'; do echo -n .; sleep 2; done;
 sleep 5
 echo " ##### RUNNING HBASE TABLE LIST TEST ##### "
