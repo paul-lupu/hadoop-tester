5e2a03433ff5714ff0d5d5f318d0b5acb0b1d524
paul-lupu <lupugeorgepaul@gmail.com>
2016-01-19 15:46:42 +0200

    update sqoop test

--- tests_ht/files/sqoop/sqoop_test.sh
+++ tests_ht/files/sqoop/sqoop_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "##### TESTING SQOOP EXPORT #####"
-mysql -u root -e 'use test; create table sample_07 (code text, description text, total_emp int, salary int);'
-sqoop export --connect jdbc:mysql://127.0.0.1/test --username hive --password hive --table sample_07 --export-dir /apps/hive/warehouse/sample_07
+mysql -u root -e 'use test; create table sample_07 (code text, description text, total_emp long, salary long);'
+sqoop export --connect jdbc:mysql://127.0.0.1/test --username hive --password hive --table sample_07 --export-dir /apps/hive/warehouse/sample_07 --driver com.mysql.jdbc.Driver
 if [ $? -ne 0 ]; then
 	exit 1
 fi
