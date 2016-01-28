cc49c4676d215e1d481024d8005f582896184b50
paul-lupu <lupugeorgepaul@gmail.com>
2016-01-28 12:58:15 +0200

    removed hbase start command because it will be added on jenkins test jobs for all services

--- tests_ht/files/hbase/hbase_test.sh
+++ tests_ht/files/hbase/hbase_test.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-echo ""
-echo  " ##### SENDING HBASE START COMMAND AND WAITING FOR SERVICE TO START #####"
-
-curl  -s -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo":{"context":"Turn Off Maintenance Mode"},"Body":{"ServiceInfo":{"maintenance_state":"OFF"}}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE
-sleep 5
-curl  -s -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"ServiceInfo": {"state" : "STARTED"}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE
-until /usr/bin/curl -s --user admin:admin -H "X-Requested-By: ambari" http://localhost:8080/api/v1/clusters/Sandbox/services/HBASE | grep -P '"CRITICAL" : 0'; do echo -n .; sleep 2; done;
-sleep 60
 echo " ##### RUNNING HBASE TABLE LIST TEST ##### "
 timeout 300s /usr/bin/hbase shell /opt/tests/hbase/hbase_commands
 if [ $? -eq 0 ]; then
