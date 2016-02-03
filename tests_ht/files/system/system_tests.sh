747682494804b419fe1d115131960453ca05790b
paul-lupu <lupugeorgepaul@gmail.com>
2016-02-03 16:15:58 +0200

    updated password for testing admin:admin

--- tests_ht/files/system/system_tests.sh
+++ tests_ht/files/system/system_tests.sh
@@ -2,7 +2,7 @@
 #this file will start up all services and check that everything is able to start correctly
 
 echo  "##### WAITING ON GUEST TO BECOME READY #####"
-TO=0; until curl -s -u admin:admin -i -H 'X-Requested-By: ambari' http://localhost:8080/api/v1/hosts/sandbox.hortonworks.com/ | grep host_status  | grep HEALTHY| grep -v UNHEALTHY &> /dev/null || [ $TO -eq 180 ] ; do sleep 1 $(( TO++ )); done
+TO=0; until curl -s -u admin:4o12t0n -i -H 'X-Requested-By: ambari' http://localhost:8080/api/v1/hosts/sandbox.hortonworks.com/ | grep host_status  | grep HEALTHY| grep -v UNHEALTHY &> /dev/null || [ $TO -eq 180 ] ; do sleep 1 $(( TO++ )); done
 
 if [ $TO -eq 180 ]; then
  echo "Host state is unhealthy. Exiting."
@@ -10,14 +10,14 @@ if [ $TO -eq 180 ]; then
 fi
 
 echo "##### STARTING ALL SERVICES ##### "
-for i in $(curl -s -u admin:admin -H "X-Requested-By: ambari" http://localhost:8080/api/v1/clusters/Sandbox/services/ | grep  -oP "[A-Z]{3,}" | uniq); do
-        curl  -s -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo":{"context":"Turn Off Maintenance Mode"},"Body":{"ServiceInfo":{"maintenance_state":"OFF"}}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/$i > /dev/null
+for i in $(curl -s -u admin:4o12t0n -H "X-Requested-By: ambari" http://localhost:8080/api/v1/clusters/Sandbox/services/ | grep  -oP "[A-Z]{3,}" | uniq); do
+        curl  -s -u admin:4o12t0n -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo":{"context":"Turn Off Maintenance Mode"},"Body":{"ServiceInfo":{"maintenance_state":"OFF"}}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/$i > /dev/null
         sleep 10
-        curl  -s -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"ServiceInfo": {"state" : "STARTED"}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/$i > /dev/null
+        curl  -s -u admin:4o12t0n -i -H 'X-Requested-By: ambari' -X PUT -d '{"ServiceInfo": {"state" : "STARTED"}}'  http://localhost:8080/api/v1/clusters/Sandbox/services/$i > /dev/null
 done
 
 TO=0;
-until curl -s -u admin:admin -i -H 'X-Requested-By: ambari' http://localhost:8080/api/v1/hosts/sandbox.hortonworks.com/ | grep host_status  | grep HEALTHY| grep -v UNHEALTHY &> /dev/null || [ $TO -eq 180 ] ; do sleep 1 $(( TO++ ));done
+until curl -s -u admin:4o12t0n -i -H 'X-Requested-By: ambari' http://localhost:8080/api/v1/hosts/sandbox.hortonworks.com/ | grep host_status  | grep HEALTHY| grep -v UNHEALTHY &> /dev/null || [ $TO -eq 180 ] ; do sleep 1 $(( TO++ ));done
 if [ $TO -eq 180 ]; then
  echo "Starting all services failed. Exiting."
  exit 1
