08e2196aaf535addea6d70d533464f8cc2e1b569
paul-lupu <lupugeorgepaul@gmail.com>
2016-08-10 15:15:23 +0300

    updated sqoop export test

--- tests_ht/files/sqoop/sqoop_test.sh
+++ tests_ht/files/sqoop/sqoop_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "##### TESTING SQOOP EXPORT #####"
-mysql -u root -phadoop -e 'use test; create table sample_07 (code text, description text, total_emp long, salary long);'
+mysql -u root -phadoop -e 'create database test; use test; create table sample_07 (code text, description text, total_emp long, salary long);'
 sqoop export --connect jdbc:mysql://127.0.0.1/test --username hive --table sample_07 --fields-terminated-by '\t' --export-dir /apps/hive/warehouse/sample_07 --driver com.mysql.jdbc.Driver
 if [ $? -ne 0 ]; then
 	exit 1
