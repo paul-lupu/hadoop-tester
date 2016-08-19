81b7bf0aeed53b3217709b94b86b3b9f42f967d4
paul-lupu <lupugeorgepaul@gmail.com>
2016-08-19 18:39:19 +0300

    added fail condition after each test

--- tests_ht/files/all_tests.sh
+++ tests_ht/files/all_tests.sh
@@ -2,6 +2,10 @@
 
 cd /opt/tests
 /bin/bash ./system/system_tests.sh
+    if [ $? -ne 0 ]; then echo '!!!!! INTEGRATION TEST FAILED. EXITING...'; exit 1; fi;
 /bin/bash ./api_tests.sh
+    if [ $? -ne 0 ]; then echo '!!!!! INTEGRATION TEST FAILED. EXITING...'; exit 1; fi;
 /bin/bash ./service_tests.sh
+    if [ $? -ne 0 ]; then echo '!!!!! INTEGRATION TEST FAILED. EXITING...'; exit 1; fi;
+
 
