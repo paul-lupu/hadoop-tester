d9b1936277151a6651f5f6f4f9ec40ee9882af0c
paul-lupu <lupugeorgepaul@gmail.com>
2016-11-02 20:34:29 +0200

    working version of the zeppelin test script

--- tests_ht/files/zeppelin/zeppelin.sh
+++ tests_ht/files/zeppelin/zeppelin.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 echo " ##### RUNNING ZEPPELIN NOTEBOOKS TEST ##### "
-for i in 2B48PF7SN 2BFGYS3YT 2BJVW65WS 2BNDT63TY 2BEQE47HR; do 
-	echo "Running notebook ID: $i ..."
-	if [[ $(/bin/bash /opt/tests/zeppelin/zeptest.sh $i  | grep OK ) ]]; 
-		then 
-			echo "Run completed successfully!" 
-			echo ""
-	else
-		echo "Zeppelin notebook ID: $i failed to execute! Aborting..."
-		return 1;
-	fi;
+for i in 2BEQE47HR; do
+  echo "";
+  echo "Running parahraphs for note: $i":
+  curl -s -XGET http://localhost:9995/api/interpreter/setting | jq '.body[] | .id'
+  interpreter_settings_ids=`curl -s -XGET http://localhost:9995/api/interpreter/setting | jq '.body[] | .id'`
+  id_array="["`echo ${interpreter_settings_ids} | tr ' ' ','`"]"
+  curl -s -XPUT -d $id_array http://localhost:9995/api/notebook/interpreter/bind/$i&> /dev/null
+
+  for y in $(curl  -s -XGET http://localhost:9995/api/notebook/job/$i | grep  -oP [0-9]+-[0-9]+_[0-9]+); do
+    echo -n "Running paragraph $y:";
+    if [[ $(curl -s -XPOST http://localhost:9995/api/notebook/job/$i/$y | grep OK) ]]; then
+      echo -n "Waiting for results "; while curl  -s -XGET http://localhost:9995/api/notebook/job/$i | grep 'PENDING\|RUNNING' >/dev/null; do echo -n "."; sleep 1; done;
+      if [[ $(curl  -s -XGET http://localhost:9995/api/notebook/job/$i | grep ERROR ) ]]; then
+        echo "Failed! Exitting...";
+        exit 1;
+      fi
+      echo -n "Complete! ";
+    else
+      echo "Couldn't run paragraph, exitting...";
+      exit 1;
+    fi
+  done
+  echo ""
 done
 echo "All zeppelin tests successfull!"
 
